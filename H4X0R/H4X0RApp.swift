49e9d061867707a745deccdba175d1e402de13cb
Maksim Stogniy <mstogniy@ya.ru>
2024-04-13 12:49:29 +0300

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by  Maksim Stogniy on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
